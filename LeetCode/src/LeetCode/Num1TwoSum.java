e71380197df847c28626cfb24ee4a2521096794c
YantaiGao <gaoyantai_jsj@163.com>
2016-04-25 09:44:47 +0800

    solution

--- LeetCode/src/LeetCode/Num1TwoSum.java
+++ LeetCode/src/LeetCode/Num1TwoSum.java
@@ -1,5 +1,20 @@
 package LeetCode;
 
+/**
+ *
+ * @author Administrator
+ * 
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution.
+ * 
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * 
+ *
+ */
+
 public class Num1TwoSum {
 
 	public static void main(String[] args) {
