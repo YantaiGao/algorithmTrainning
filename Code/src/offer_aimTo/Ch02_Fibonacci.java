8dd71562381616da294cef032a5819b50fe90b0b
GaoYantai <gaoyantai@gmail.com>
2016-08-30 19:24:46 +0800

    Fibonacci

---
+++ Code/src/offer_aimTo/Ch02_Fibonacci.java
@@ -0,0 +1,22 @@
+package offer_aimTo;
+
+/**
+ * 求斐波那契数列第n项,或者打印前n项
+ * 
+ * @author lenovo
+ *
+ */
+public class Ch02_Fibonacci {
+
+	public static void main(String[] args) {
+		int n = 10;
+		Ch02_Fibonacci fibo = new Ch02_Fibonacci();
+		fibo.findN(n);
+	}
+
+	private void findN(int n) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
