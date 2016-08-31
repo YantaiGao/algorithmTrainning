ad7945221010e96f9b2b6c358a5b77460681cb99
GaoYantai <gaoyantai@gmail.com>
2016-08-31 13:20:00 +0800

    Fibo && UpstirsUpdate

--- Code/src/acmcoder/No3_UpStairs.java
+++ Code/src/acmcoder/No3_UpStairs.java
@@ -24,22 +24,67 @@ public class No3_UpStairs {
 	 * 
 	 * 存在的问题：当台阶数达到46以上甚至50 的时候，算法将会越来越慢
 	 * 
+	 * 类似fibo数列，只所以慢是因为使用了递归，有大量的重复计算
+	 * 优化从下向上求
+	 * 
+	 * 注意：对于普通解决思路中，设置是刚开始的时候在第一阶台阶上
+	 * 对于优化思路来说，假设刚开始在0台阶上
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
 		int groupNum,m,res;
+		
 		Scanner sc = new Scanner(System.in);
+		
 		groupNum = sc.nextInt();
+		
 		No3_UpStairs upStairs = new No3_UpStairs();
+		
 		for (int i = 0; i < groupNum; i++) {
 			m = sc.nextInt();
 			res = upStairs.computeMethodsNum(m);
 			System.out.println(res);
 		}
+		//以上是剑指offer上问题，下边是优化后的方法,题设也稍微有修改
+		int n = 10;
+		long res2 = upStairs.computeFromDown(n);
+		System.out.print(res2+"\t");
+		
 	}
 
 	/**
-	 * 注意要有m == 0 和 m == 3
+	 * 注：
+	 * 此时题设修改，在台阶0上
+	 * 时间复杂度：o(n)
+	 * @param m
+	 * @return
+	 */
+	private long computeFromDown(int m) {
+		if (m == 0) {
+			return 0;
+		}else if (m==1) {
+			return 1;
+		}else if (m==2) {
+			return 2;
+		}else {
+			long resN = 0;
+			long numFront = 2;
+			long numBehind = 1;
+			for (int i = 3; i <= m; i++) {
+				resN = numFront + numBehind;
+				numBehind = numFront;
+				numFront = resN;
+			}
+			return resN;
+		}
+	}
+
+	/**
+	 * 注1：要有m == 0 和 m == 3
+	 * 注2：
+	 * 刚开始在第一阶台阶，m=2上一个台阶就好
 	 * @param m
 	 * @return
 	 */
--- Code/src/offer_aimTo/Ch02_Fibonacci.java
+++ Code/src/offer_aimTo/Ch02_Fibonacci.java
@@ -1,7 +1,22 @@
 package offer_aimTo;
 
 /**
- * 求斐波那契数列第n项,或者打印前n项
+ * 求：斐波那契数列第n项,或者打印前n项
+ * 相关题目，青蛙上台阶
+ * 公式：
+ * f(0)=0;
+ * f(1)=1;
+ * f(n)=f(n-1)+f(n-2)
+ * 
+ * 普通思路：递归，从上到下
+ * 但是：这种递归方式会有很多重复计算
+ * eg:
+ * f(10) = f(9)+f(8)
+ * f(9) = f(8)+f(7)
+ * f(8) = f(7)+f(6)
+ * 
+ * 优化思路：
+ * 避免重复计算，从下到上计算
  * 
  * @author lenovo
  *
@@ -9,14 +24,70 @@ package offer_aimTo;
 public class Ch02_Fibonacci {
 
 	public static void main(String[] args) {
-		int n = 10;
+		int n = 50;
+		int res;
 		Ch02_Fibonacci fibo = new Ch02_Fibonacci();
-		fibo.findN(n);
+//		for (int i = 0; i <= n; i++) {
+//			res = fibo.findN(i);
+//			System.out.print(res+"\t");	
+//			if (i%10 == 0 && i!=0) {
+//				System.out.println();
+//			}
+//		}
+		
+		long res2 = 0;
+		for (int i = 0; i <= n; i++) {
+			res2 = fibo.findNFromDown(i);
+			System.out.print(res2 + "\t");
+			if (i%10 == 0 && i!=0) {
+				System.out.println();
+			}
+		}
 	}
 
-	private void findN(int n) {
-		// TODO Auto-generated method stub
+	/**
+	 * 传统的递归，由上到下递归
+	 * 易理解，但是低效，重复计算多
+	 * @param n
+	 * @return
+	 */
+	private int findN(int n) {
+		if (n==0) {
+			return 0;
+		}else if(n==1){
+			return 1;
+		}else {
+			return findN(n-1)+findN(n-2);
+		}
+	}
+
+	/**
+	 * 优化1：
+	 * 避免从上到下的重复计算，从下到上计算
+	 * 时间复杂度o(n)
+	 * @param n
+	 * @return
+	 */
+	private long findNFromDown(int n) {
+		if (n==0) {
+			return 0;
+		}else if(n==1){
+			return 1;
+		}else {
+			
+			long fibo1 = 1;
+			long fibo2 = 0;
+			
+			long fiboN = 0;
+			
+			for (int i = 0; i < n; i++) {
+				fiboN = fibo1 + fibo2;
+				fibo2 = fibo1;
+				fibo1 = fiboN;
+			}
+			
+			return fiboN;
+		}
 		
 	}
-
 }
