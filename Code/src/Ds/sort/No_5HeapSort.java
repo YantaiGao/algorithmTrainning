9544bc1d46b9b81d6595a9d72da23cf4920ca329
YantaiGao <gaoyantai_jsj@163.com>
2016-09-04 22:24:09 +0800

    Sort

---
+++ Code/src/Ds/sort/No_5HeapSort.java
@@ -0,0 +1,27 @@
+package Ds.sort;
+/**
+ * 堆排序
+ * 1:介绍
+ * （1）
+ * 
+ * 2：时间复杂度
+ * （1）建堆 O(n)
+ * （2）排序logn
+ * （3）总时间复杂度：nlogn
+ * 
+ * 3:空间复杂度
+ * （1）O(1)每次对一个元素进行操作，只需要一个辅存
+ * 
+ * 4：稳定性：
+ * 不稳定性排序
+ * 
+ * @author Administrator
+ *
+ */
+public class No_5HeapSort {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
